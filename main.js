6514dd4894189af57619636785041fe8b6510523
Joebim <josey359@gmail.com>
2021-09-04 06:49:41 +0100

    My first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+const hamburger = document.querySelector(".hamburger")
+const navLink = document.querySelector(".nav-link-wrapper")
+
+hamburger.addEventListener('click', () => {
+    navLink.classList.toggle('show')
+    console.log('testing')
+})
+
+const cardTwo = document.querySelector(".card-two")
+function btn_1_click() {
+    cardTwo.classList.toggle('show')
+    console.log('testing')
+}
+
+const cardThree = document.querySelector(".card-three")
+function btn_2_click() {
+    cardThree.classList.toggle('show')
+    console.log('testing')
+}
+
+const cardFour = document.querySelector(".card-four")
+function btn_3_click() {
+    cardFour.classList.toggle('show')
+    console.log('testing')
+}
+
+const cardFive = document.querySelector(".card-five")
+function btn_4_click() {
+    cardFive.classList.toggle('show')
+    console.log('testing')
+}
+
+const cardSix = document.querySelector(".card-six")
+function btn_5_click() {
+    cardSix.classList.toggle('show')
+    console.log('testing')
+}
+
+const cardSeven = document.querySelector(".card-seven")
+function btn_6_click() {
+    cardSeven.classList.toggle('show')
+    console.log('testing')
+}
+// info.addEventListener('click', () => {
+//     cardThree.classList.toggle('show')
+//     console.log('testing')
+// })
+
+// info.addEventListener('click', () => {
+//     cardFour.classList.toggle('show')
+//     console.log('testing')
+// })
+
+// info.addEventListener('click', () => {
+//     cardFive.classList.toggle('show')
+//     console.log('testing')
+// })
+
+// info.addEventListener('click', () => {
+//     cardSix.classList.toggle('show')
+//     console.log('testing')
+// })
+
+// info.addEventListener('click', () => {
+//     cardSeven.classList.toggle('show')
+//     console.log('testing')
+// })
